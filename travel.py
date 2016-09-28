49e0c77a26cf27350c131975f3c354032b89aa31
Regan <reganchan@fortinet.com>
2016-09-27 18:06:32 -0700

    crappy solution

---
+++ travel.py
@@ -0,0 +1,69 @@
+import sys
+
+travels = [ [ 0 ] * 100 for i in range(100) ]
+cities  = {}
+line = raw_input( "city1 city2?" )
+while line:
+  cityn1, cityn2 = line.split()
+  if cityn1 in cities:
+    city1 = cities[cityn1]
+  else:
+    city1 = len(cities)
+    cities[cityn1] = city1
+
+  if cityn2 in cities:
+    city2 = cities[cityn2]
+  else:
+    city2 = len(cities)
+    cities[cityn2] = city2
+
+  travels[city1][city2] += 1
+  line = raw_input( "city1 city2 (enter if no more)?" )
+
+startcity = []
+for y in range( len(cities) ):
+  if not any([travels[x][y] for x in range( len(cities) )]):
+    startcity.append(y)
+
+citynames = {x[1]: x[0] for x in cities.iteritems()}
+if 1!=len(startcity):
+  print "multiple start cities!"
+  sys.exit()
+
+startcity = startcity[0]
+print "start city:", citynames[ startcity ]
+
+def traverse( city, printstr ):
+  printstr += "%s" % citynames[ city ]
+  nextcities = travels[ city ]
+  nextcitynos = [ i for i in range(len(cities)) if nextcities[i] > 0 ]
+  if len( nextcitynos ) == 1:
+    nextcityno = nextcitynos[0]
+    nextcities[ nextcityno ] -= 1
+    traverse( nextcityno, printstr + " -> " )
+    return
+
+  print printstr
+  for nextcityno in nextcitynos:
+    if nextcities[ nextcityno ] < 1:
+      continue
+    nextcities[ nextcityno ] -= 1
+    traverse( nextcityno, "(%s) -> " % citynames[city] )
+
+traverse( startcity, "" )
+
+"""
+SFO LAX 
+LAX ATL 
+ATL DEN 
+DEN BOS 
+BOS JFK 
+JFK DEN 
+DEN PHL 
+PHL DFW 
+DFW SEA 
+SEA DEN 
+DEN HOU 
+HOU MIA
+"""
+
